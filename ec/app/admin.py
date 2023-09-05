07d90399e8c7bfdcd901021b22c916e836bb7877
Prashant Jagtap <121176475+prashant2130@users.noreply.github.com>
2023-09-05 10:20:12 +0530

    first commit

---
+++ ec/app/admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.urls import reverse
+from django.utils.html import format_html
+from . models import Cart, Customer, OrderPlaced, Payment, Product, Wishlist
+# Register your models here.
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id','title','discounted_price','category','product_image']
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','locality','city','state', 'zipcode']
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','product','quantity']
+    def products(self,obj):
+        link=reverse("admin:app_product_change",args=[obj.product.pk])
+        return format_html('<a href="{}">{}</a>',link,obj.product.title)
+
+@admin.register(Payment)
+class PaymentModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','amount','razorpay_order_id','razorpay_payment_status','razorpay_payment_id','paid']
+
+@admin.register(OrderPlaced)
+class OrderPlacedModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','customers','products','quantity','ordered_date','status','payments']
+    def customers(self,obj):
+        link=reverse("admin:app_customer_change",args=[obj.customer.pk])
+        return format_html('<a href="{}">{}</a>',link,obj.customer.name)
+
+    def products(self,obj):
+        link=reverse("admin:app_product_change",args=[obj.product.pk])
+        return format_html('<a href="{}">{}</a>',link,obj.product.title)
+
+    def payments(self,obj):
+        link=reverse("admin:app_payment_change",args=[obj.payment.pk])
+        return format_html('<a href="{}">{}</a>',link,obj.payment.razorpay_payment_id)
+
+@admin.register(Wishlist)
+class WishlistModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','products']
+    def products(self,obj):
+        link=reverse("admin:app_product_change",args=[obj.product.pk])
+        return format_html('<a href="{}">{}</a>',link,obj.product.title)
+
+admin.site.unregister(Group)
